b7c2ac00f153be3f50c9ee7c9173f3d8201f1eb2
Ericka Allen <erickaallen@macbook-pro.home>
2018-09-13 22:02:59 -0400

    Create inputs and connect them to state with event handlers

--- calendar-project/src/App.js
+++ calendar-project/src/App.js
@@ -1,8 +1,62 @@
 import React, { Component } from 'react';
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+// import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getFormattedDate = this.getFormattedDate.bind(this);
+    this.handleStartDateChange = this.handleStartDateChange.bind(this);
+    this.handleNumberOfDaysChange = this.handleNumberOfDaysChange.bind(this);
+    this.handleCountryCodeChange = this.handleCountryCodeChange.bind(this);
+
+    this.state = {
+      startDate: this.getFormattedDate(),
+      numberDays: 30,
+      countryCode: ""
+    };
+  }
+
+  getFormattedDate() {
+    let rawDate = new Date();
+    let year = rawDate.getFullYear().toString();
+    let rawMonth = (rawDate.getMonth() + 1).toString();
+    let month;
+    if (rawMonth.length === 1) {
+      month = "0" + rawMonth;
+    } else {
+      month = rawMonth;
+    }
+    let date = rawDate.getDate().toString();
+    let formattedDate = year + "-" + month + "-" + date;
+    console.log(formattedDate);
+    return formattedDate;
+  }
+
+  handleStartDateChange(event) {
+    console.log("date changed!");
+    this.setState({
+      startDate: event.target.value
+    });
+  }
+
+  handleNumberOfDaysChange(event) {
+    console.log("number of days changed!");
+    this.setState({
+      numberDays: event.target.value
+    });
+  }
+
+  handleCountryCodeChange(event) {
+    console.log("country code changed!");
+    this.setState({
+      countryCode: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +64,33 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form className="">
+          <FormGroup>
+            <ControlLabel>Start Date</ControlLabel>
+            <FormControl 
+              type="date"
+              value={this.state.startDate}
+              onChange={this.handleStartDateChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Number of Days</ControlLabel>
+            <FormControl 
+              type="number" 
+              value={this.state.numberDays}
+              onChange={this.handleNumberOfDaysChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Country Code</ControlLabel>
+            <FormControl 
+              type="text" 
+              value={this.state.countryCode}
+              placeholder="Enter your country code"
+              onChange={this.handleCountryCodeChange}
+            />
+          </FormGroup>
+        </form>
       </div>
     );
   }
